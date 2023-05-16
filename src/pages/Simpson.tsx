839f3c2a2d36b1fa76445c887a4315384d35deae
Ben-Sicat <bensicat00@gmail.com>
2023-05-16 23:09:43 +0800

    functons added; "
    "
    ""

--- src/pages/Simpson.tsx
+++ src/pages/Simpson.tsx
@@ -1,71 +1,66 @@
 import React, { useState } from 'react';
-import * as math from 'mathjs';
+import { Container, Box, Paper, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-interface Simpson {}
-
-const Simpson: React.FC<Simpson> = () => {
+const Simpson: React.FC = () => {
   const [expression, setExpression] = useState('');
   const [result, setResult] = useState('');
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+  const [n, setN] = useState('');
 
-  const SimpsonIntegration: React.FC = () => {
-    const [a, setA] = useState('');
-    const [b, setB] = useState('');
-    const [n, setN] = useState('');
-    const [result, setResult] = useState('');
+  const deltaX = (x1: number, x2: number, n1: number) => {
+    return (x1 - x2) / n1;
+  };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      const result = math.evaluate(`simpson(${a}, ${b}, ${n}, x)`, { x: 1 });
-      setResult(result);
-    };
+  const simpsonCalc = (a: number, b: number, n: number, expression: string) => {
+    let sum1 = 0;
+    let sum2 = 0;
+    let delta = deltaX(b, a, n);
+    let xi, fi;
 
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            a:
-            <input
-              type="text"
-              value={a}
-              onChange={(e) => setA(e.target.value)}
-            />
-          </label>
-          <label>
-            b:
-            <input
-              type="text"
-              value={b}
-              onChange={(e) => setB(e.target.value)}
-            />
-          </label>
-          <label>
-            n:
-            <input
-              type="text"
-              value={n}
-              onChange={(e) => setN(e.target.value)}
-            />
-          
-          </label>
-          <label>
-            f(x)
-            <input
-              type="text"
-              value={expression}
-              onChange={(e) => setExpression(e.target.value)}
-            />
-          </label>
+    for (let i = 1; i < n; i++) {
+      xi = a + i * delta;
+      fi = eval(expression.replace('x', xi.toString())); 
 
-          <input type="submit" value="Submit" />
-        </form>
-        <p>Result: {result}</p>
-      </div>
-    );
+      if (i % 2 === 0) {
+        sum2 += fi;
+      } else {
+        sum1 += fi;
+      }
+    }
+
+    let result = (delta / 3) * (eval(expression.replace('x', a.toString())) + eval(expression.replace('x', b.toString())) + 2 * sum2 + 4 * sum1);
+    setResult(result.toString());
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    simpsonCalc(parseFloat(a), parseFloat(b), parseInt(n), expression);
   };
 
   return (
     <div>
-      <SimpsonIntegration />
+      <form onSubmit={handleSubmit}>
+        <label>
+          a:
+          <input type="text" value={a} onChange={(e) => setA(e.target.value)} />
+        </label>
+        <label>
+          b:
+          <input type="text" value={b} onChange={(e) => setB(e.target.value)} />
+        </label>
+        <label>
+          n:
+          <input type="text" value={n} onChange={(e) => setN(e.target.value)} />
+        </label>
+        <label>
+          f(x)
+          <input type="text" value={expression} onChange={(e) => setExpression(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <p>Result: {result}</p>
     </div>
   );
 };
--- src/pages/Trapezoid.tsx
+++ src/pages/Trapezoid.tsx
@@ -1,19 +1,61 @@
-import React from 'react'
-import { Container, Box, Paper, Button, Typography} from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Box, Paper, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 
 const Trapezoid: React.FC = () => {
-const [expression, setExpression] = useState('');
-const [result, setResult] = useState('');
-const [a, setA] = useState('');
-const [b, setB] = useState('');
-const [n, setN] = useState(''); 
+  const [expression, setExpression] = useState('');
+  const [result, setResult] = useState('');
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+  const [n, setN] = useState('');
 
-    
+  const deltaX = (x1: number, x2: number, n1: number) => {
+    return (x1 - x2) / n1;
+  };
+
+  const trapezoidCalc = (a: number, b: number, n: number, expression: string) => {
+    let sum = 0;
+    let delta = deltaX(b, a, n);
+
+    for (let i = 1; i < n; i++) {
+      let xi = a + i * delta;
+      let fi = eval(expression.replace('x', xi.toString())); 
+      sum += fi;
+    }
+
+    let result = delta * (eval(expression.replace('x', a.toString())) + eval(expression.replace('x', b.toString()))) / 2 + delta * sum;
+    setResult(result.toString());
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    trapezoidCalc(parseFloat(a), parseFloat(b), parseInt(n), expression);
+  };
+
   return (
-    <div>Trapezoid</div>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          a:
+          <input type="text" value={a} onChange={(e) => setA(e.target.value)} />
+        </label>
+        <label>
+          b:
+          <input type="text" value={b} onChange={(e) => setB(e.target.value)} />
+        </label>
+        <label>
+          n:
+          <input type="text" value={n} onChange={(e) => setN(e.target.value)} />
+        </label>
+        <label>
+          f(x)
+          <input type="text" value={expression} onChange={(e) => setExpression(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <p>Result: {result}</p>
+    </div>
+  );
+};
 
-export default Trapezoid
+export default Trapezoid;
